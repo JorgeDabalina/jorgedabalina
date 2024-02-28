631f45c9d32b58a4f56d9361244141e92ca2710d
Jorge Dabaliña <jorgedabalina@gmail.com>
2024-02-28 16:03:49 +0100

    hello

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/index.css';
+import { hydrate, render } from "react-dom";
+
 
 import Main from './Main';
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-root.render(
-  <React.StrictMode>
-      <Main />
-  </React.StrictMode>
-);
+if (rootElement.hasChildNodes()) {
+  hydrate(<Main />, rootElement);
+} else {
+  render(<Main />, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
