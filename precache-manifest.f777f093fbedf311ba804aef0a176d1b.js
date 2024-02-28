76b0ee591b19bbabed90e1dc39fce0194fef995a
Jorge Dabaliña <jorgedabalina@gmail.com>
2024-02-28 16:36:19 +0100

    Updates

---
+++ precache-manifest.f777f093fbedf311ba804aef0a176d1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed964980fc11cb77eb7f",
+    "url": "/static/css/main.cffaa098.chunk.css"
+  },
+  {
+    "revision": "ed964980fc11cb77eb7f",
+    "url": "/static/js/main.7a3a12d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e984633128973cd12dbf",
+    "url": "/static/css/2.ca75a3ed.chunk.css"
+  },
+  {
+    "revision": "e984633128973cd12dbf",
+    "url": "/static/js/2.43617c23.chunk.js"
+  },
+  {
+    "revision": "ba49e844892321d8540ea3b7c088cf97",
+    "url": "/static/media/bootstrap-icons.ba49e844.woff"
+  },
+  {
+    "revision": "cc1e5eda776be5f0ff614285c31d4892",
+    "url": "/static/media/bootstrap-icons.cc1e5eda.woff2"
+  },
+  {
+    "revision": "899e895bc942ad9f9d7c2212646cffcb",
+    "url": "/index.html"
+  }
+];
